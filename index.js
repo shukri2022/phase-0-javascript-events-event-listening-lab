e6f8fcc474822e88815a3efc0602789c036572c7
Shukri Abdi <shukri751@gmail.com>
2024-07-13 01:44:36 +0300

    phase-0-javascript-events-event-listening-lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+  const input = document.getElementById('button');
+
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+
+  input.addEventListener('click', clickAlert);
 }
+
+// Call the function to activate the event listener
+addingEventListener();
